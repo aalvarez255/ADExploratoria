6d7fb9c5728ce06f3cd9c7ed06d07e2105dfbfff
Adrian <adrian.alvarez.guerra@est.fib.upc.edu>
2014-11-19 11:05:27 +0100

    adri

---
+++ app/src/main/java/com/exploratoria/adexploratoria/DisplayAdvice.java
@@ -0,0 +1,13 @@
+package com.exploratoria.adexploratoria;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class DisplayAdvice extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
